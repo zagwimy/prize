0cc0fb3c498c43eebee37da0a2110f6fee055aa6
zagwi <zagwi@vip.qq.com>
2020-08-12 12:06:57 +0800

    摇号系统sql导入

---
+++ prizeOnline/src/main/resources/prize.sql
@@ -0,0 +1,116 @@
+/*
+SQLyog Ultimate v12.14 (64 bit)
+MySQL - 5.7.24 : Database - prize
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`prize` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `prize`;
+
+/*Table structure for table `blacklist` */
+
+DROP TABLE IF EXISTS `blacklist`;
+
+CREATE TABLE `blacklist` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `blacklist` */
+
+insert  into `blacklist`(`id`,`name`) values 
+(1,'李四1'),
+(2,'李四2');
+
+/*Table structure for table `history` */
+
+DROP TABLE IF EXISTS `history`;
+
+CREATE TABLE `history` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `version` int(11) DEFAULT NULL,
+  `number` varchar(30) DEFAULT NULL,
+  `insertTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `batch` int(11) DEFAULT NULL,
+  `isdelete` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6499 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `history` */
+
+/*Table structure for table `historybackup` */
+
+DROP TABLE IF EXISTS `historybackup`;
+
+CREATE TABLE `historybackup` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `data` mediumtext,
+  `version` int(11) DEFAULT NULL,
+  `inserttime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `historybackup` */
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `remark` varchar(1000) DEFAULT NULL,
+  `number` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1132 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `student` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) DEFAULT NULL,
+  `password` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`username`,`password`) values 
+(1,'admin','20So%U%y9QA2LkpWtPRAiUkXj4&VqdKd');
+
+/*Table structure for table `whitelist` */
+
+DROP TABLE IF EXISTS `whitelist`;
+
+CREATE TABLE `whitelist` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `whitelist` */
+
+insert  into `whitelist`(`id`,`name`) values 
+(1,'张三1'),
+(2,'张三2'),
+(3,'张三3');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
